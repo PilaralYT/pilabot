269653dca1a94768241c5bfb25a873ce9b896f0b
Pilaral <120129058+PilaralYT@users.noreply.github.com>
2024-11-20 22:48:53 +0300

    Update print.js

--- plugins/print.js
+++ plugins/print.js
@@ -1,1 +1,19 @@
+const {
+  bot,
+} = require('../lib/');
+
+bot(
+  {
+    pattern: 'print ?(.*)',
+    desc: 'Print a message prefixed with [PilaBot]',
+    type: 'utility',
+  },
+  async (message, match) => {
+    if (!match) {
+      return await message.send('*Usage: .print <message>*');
+    }
+    const response = `[PilaBot] ${match}`;
+    return await message.send(response);
+  }
+);
 
